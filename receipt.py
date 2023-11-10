2ca911ddca19ccee2e76663959dfe824b835aa6b
Muhd Syahir <96367612+TheRealSyahir@users.noreply.github.com>
2023-11-10 14:35:46 +0800

    Create receipt.py

---
+++ receipt.py
@@ -0,0 +1,39 @@
+#mini project on receipts
+lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+
+lovely_loveseat_price = 254.00
+
+stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+
+stylish_settee_price = 180.50
+
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+
+luxurious_lamp_price = 52.15
+
+#8.8 sales tax
+sales_tax = 0.088
+
+customer_one_total = 0
+customer_one_itemization = ""
+
+#first customer buying lovely loveseat
+customer_one_total += lovely_loveseat_price
+#print("$" + str(customer_one_total))
+customer_one_itemization += lovely_loveseat_description
+#print(customer_one_itemization)
+#first customer adding to purchase luxury lamp as well
+customer_one_total +=luxurious_lamp_price
+#print(customer_one_total)
+customer_one_itemization += "\n" + luxurious_lamp_description
+#print(customer_one_itemization)
+
+#find sales tax for customer one'
+customer_one_tax = customer_one_total * sales_tax
+
+customer_total_cost = round(customer_one_total + customer_one_tax, 2)
+#Print receipt for customer one
+print("Customer one Items: ")
+print(customer_one_itemization)
+print("Customer One Total: ")
+print("$" + str(customer_total_cost))
